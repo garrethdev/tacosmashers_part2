f92e89d572a321271272207a80e65a94db0ad5bd
Strand McCutchen <strand@bettermistak.es>
2013-10-28 12:33:53 -0700

    Require elephant model in the app

--- app/app.rb
+++ app/app.rb
@@ -1,6 +1,7 @@
 class SinatraApp < Sinatra::Base
   require 'sinatra'
   require 'active_record'
+  require_relative './models/elephant'
 
   ActiveRecord::Base.establish_connection(adapter: 'postgresql')
 
