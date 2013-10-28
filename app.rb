4c01ed5e197816b043a12ee3a79538a2b486a76f
Strand McCutchen <strand@bettermistak.es>
2013-10-28 15:41:06 -0700

    explicitly include db in establish_connection

--- app.rb
+++ app.rb
@@ -2,7 +2,8 @@ require 'sinatra'
 require 'active_record'
 require_relative './app/models/elephant'
 
-ActiveRecord::Base.establish_connection(adapter: 'postgresql')
+ActiveRecord::Base.establish_connection(adapter: 'postgresql',
+                                        database: 'sinatra_skeleton_dev')
 
 get '/' do
   "Hello World!"
