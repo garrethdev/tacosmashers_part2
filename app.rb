e83768e5b650689d3d631c327cd6eb78ef101540
Apprentice <apprentice@dbc-25.local>
2013-10-30 18:14:52 -0700

    posts save and display. search functionality in progress.

--- app.rb
+++ app.rb
@@ -3,9 +3,33 @@ require 'active_record'
 require_relative './app/models/member'
 require_relative './app/models/post'
 
-
 ActiveRecord::Base.establish_connection(adapter: 'postgresql', database: 'time_fuck')
 
 get '/' do
-  "Welcome to Time Fuck!"
+  erb :index
+end
+
+get '/member' do
+  erb :member
+end
+
+post '/posts' do
+  p params
+  Post.create(params)
+  redirect '/member/feed'
+end
+
+get '/member/feed' do
+  @posts = Post.all
+  erb :feed
+end
+
+post '/search' do
+  p Member.find_by first_name: params[:search_entry]
+  redirect '/member'
+end
+
+post '/new_member' do
+  Member.create(params)
+  redirect '/member/feed'
 end
